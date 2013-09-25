932e3de3e3b8d9882639a785a67ae07821fee7b6
R655 <Rusty65536@gmail.com>
2013-09-25 09:01:41 +0600

    Добавлена таблица с сессиями

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130916154453) do
+ActiveRecord::Schema.define(:version => 20130925030035) do
 
   create_table "accrual_types", :force => true do |t|
     t.string  "name",          :limit => 20,                :null => false
@@ -131,6 +131,16 @@ ActiveRecord::Schema.define(:version => 20130916154453) do
   add_index "premia", ["department_id"], :name => "prem_depFK"
   add_index "premia", ["employee_id"], :name => "prem_employeesFK"
   add_index "premia", ["name"], :name => "index_premia_on_name", :unique => true
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sick_leaves", :force => true do |t|
     t.integer "employee_id",                    :null => false
